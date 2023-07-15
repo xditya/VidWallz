aead08a78e97c61a25a3aeff356dc2c7358eb951
Aditya <me@xditya.me>
2023-07-15 20:17:28 +0530

    featL select wall directly on home page

--- app/src/main/java/me/xditya/vidwallz/ui/screens/HomeScreen.kt
+++ app/src/main/java/me/xditya/vidwallz/ui/screens/HomeScreen.kt
@@ -1,70 +1,138 @@
 package me.xditya.vidwallz.ui.screens
 
+import android.app.WallpaperManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.navigation.NavController
-import kotlinx.coroutines.launch
 import me.xditya.vidwallz.ui.appbars.BottomBar
 import me.xditya.vidwallz.ui.appbars.TopBar
+import java.io.IOException
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
-    val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
-    Scaffold (
-        snackbarHost = { SnackbarHost(snackbarHostState) },
+    // for image search
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    // for wall setting
+    val ctxW = LocalContext.current as ComponentActivity
+    val launcherW = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = {
+            launcher.launch("image/*")
+        }
+    )
+
+    Scaffold(
         topBar = { TopBar() },
         bottomBar = { BottomBar(navController = navController) }
     ) {
-        Surface (
+        Surface(
             modifier = Modifier.padding(it)
         ) {
-            Column (
+            Column(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
-            ){
+            ) {
                 IconButton(onClick = {
-                    scope.launch {
-                        snackbarHostState.showSnackbar("Add wallpapers using the \"+\" icon!",
-                            actionLabel = "OK"
-                        )
+                    if (hasSetWallpaperPermission(ctxW)) {
+                        launcher.launch("image/*")
+                    } else {
+                        Toast.makeText(context, "Permission not granted", Toast.LENGTH_SHORT).show()
+                        launcherW.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    }
+                    imageUri?.let { it ->
+                        if (Build.VERSION.SDK_INT < 28) {
+                            @Suppress("DEPRECATION")
+                            bitmap.value = MediaStore.Images
+                                .Media.getBitmap(context.contentResolver, it)
+
+                        } else {
+                            val source = ImageDecoder
+                                .createSource(context.contentResolver, it)
+                            bitmap.value = ImageDecoder.decodeBitmap(source)
+                        }
+
+                        bitmap.value?.let { btm ->
+                            val wallpaperManager = WallpaperManager.getInstance(context)
+                            try {
+                                wallpaperManager.setBitmap(btm)
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
+                        }
                     }
                 }) {
-                    Icon(imageVector = Icons.Outlined.Info, contentDescription = "InfoIcon")
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add Wallpapers",
+                        Modifier.size(48.dp)
+                    )
                 }
                 Text(
-                    "Nothing here yet!",
-                    style = MaterialTheme.typography.bodyLarge,
+                    "Select Image!",
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
                     modifier = Modifier.padding(16.dp)
                 )
             }
         }
     }
+}
+
+private fun hasSetWallpaperPermission(context: ComponentActivity): Boolean {
+    val permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    val result = ContextCompat.checkSelfPermission(context, permission)
+    return result == PermissionChecker.PERMISSION_GRANTED
 }
 
 @Preview
