ec03760415bf275f9a53eede08ccb39362afde22
Aditya <me@xditya.me>
2023-07-15 19:26:42 +0530

    feat: image selector

--- app/src/main/java/me/xditya/vidwallz/ui/appbars/BottomBar.kt
+++ app/src/main/java/me/xditya/vidwallz/ui/appbars/BottomBar.kt
@@ -1,5 +1,14 @@
 package me.xditya.vidwallz.ui.appbars
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material.icons.sharp.Home
@@ -9,15 +18,35 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+
 
 @Composable
 fun BottomBar(
     navController: NavController,
 ) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val bitmap =  remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    val launcher = rememberLauncherForActivityResult(contract =
+    ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
     BottomAppBar(
         actions = {
             IconButton(onClick = {
@@ -37,8 +66,28 @@ fun BottomBar(
                         Icons.Sharp.Add, contentDescription = "add wall"
                     )
                 }, onClick = {
-                     // #TODO: Add wallpaper functions
-                })
+                    launcher.launch("image/*")
+                    }
+            )
+            imageUri?.let {
+                if (Build.VERSION.SDK_INT < 28) {
+                    @Suppress("DEPRECATION")
+                    bitmap.value = MediaStore.Images
+                        .Media.getBitmap(context.contentResolver,it)
+
+                } else {
+                    val source = ImageDecoder
+                        .createSource(context.contentResolver,it)
+                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                }
+
+                bitmap.value?.let {  btm ->
+                    Image(bitmap = btm.asImageBitmap(),
+                        contentDescription =null,
+                        modifier = Modifier.size(400.dp))
+
+                }
+            }
         }
     )
 }
